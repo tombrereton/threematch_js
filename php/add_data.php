520f8e5d6c536b2f2d9c2cde1a70d04d9e5f0556
tombrereton <tbrereton89@gmail.com>
2017-07-04 18:53:15 +0100

    Fixed php calls to write data files.

--- php/add_data.php
+++ php/add_data.php
@@ -2,7 +2,7 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $game_id = $_POST['gameID'];
     $data_string = $_POST['dataString'];
-    $file = fopen('data/' . $game_id . '.txt', 'a');
+    $file = fopen($_SERVER['DOCUMENT_ROOT'] . '/data/' . $game_id . '.txt', 'a');
     fwrite($file, $data_string . PHP_EOL);
     fclose($file);
 }
--- php/start_file.php
+++ php/start_file.php
@@ -1,7 +1,7 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $game_id = $_POST['gameID'];
-    $data_file = fopen('data/' . $game_id . '.txt', 'a');
+    $data_file = fopen($_SERVER['DOCUMENT_ROOT'] .'/data/' . $game_id . '.txt', 'a');
     $header_file = fopen('header.txt', 'r');
     while (!feof($header_file)) {
         fwrite($data_file, fgets($header_file));
