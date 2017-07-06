9c24aca3fa317d8b6d68e9328524ac61888503a5
elliottdavies <elliottdavies94@gmail.com>
2017-07-06 19:53:41 +0100

    Added debug for file path and fopen

--- php/start_file.php
+++ php/start_file.php
@@ -2,9 +2,11 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $game_id = $_POST['gameID'];
     $valid_id = preg_match('/^\d*-\d*$/', $game_id);
-    echo $_SERVER['DOCUMENT_ROOT'];
+    echo $_SERVER['DOCUMENT_ROOT'] . PHP_EOL;
     if ($valid_id) {
-        $data_file = fopen($_SERVER['DOCUMENT_ROOT'] . '/data/' . $game_id . '.txt', 'a');
+        $path = $_SERVER['DOCUMENT_ROOT'] . '/data/' . $game_id . '.txt';
+        echo $path . PHP_EOL;
+        echo $data_file = fopen($path, 'a');
 
         $block = 1;
         if (flock($data_file, LOCK_EX, $block)) {
