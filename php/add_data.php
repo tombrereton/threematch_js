f2ff1a9126c8f4d10c3a6af3c5e23466dac4849f
elliottdavies <elliottdavies94@gmail.com>
2017-07-12 16:28:28 +0100

    Changed data string validation in add_data.php

--- php/add_data.php
+++ php/add_data.php
@@ -3,7 +3,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $game_id = $_POST['gameID'];
     $data_string = $_POST['dataString'];
     $valid_id = preg_match('/^\d+-\d+$/', $game_id);
-    $valid_string = preg_match('/^((0\tstart)|([-\d\t]{1,813}))$/', $data_string);
+    $valid_string = preg_match('/^[-\d\t]{1,1000}$/', $data_string);
     if ($valid_id && $valid_string) {
         $file = fopen($_SERVER['DOCUMENT_ROOT'] . '/data/' . $game_id . '.txt', 'a');
         $block = 1;
